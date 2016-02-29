83b37ea0fac3dace77ee027f87e9a55f65265c52
John Chapman <JayC202@gmail.com>
2016-02-28 19:45:40 -0500

    Upgraded version of react/react-router and changed to hash routing in order to support altervista.org hosted version

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Router, Route, } from 'react-router';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import App from './views/AppView';
 import globalHeadersService from './services/globalHeadersService';
@@ -7,10 +8,8 @@ import globalHeadersService from './services/globalHeadersService';
 //if we have any stored headers, load those by default
 globalHeadersService.restoreHeaders();
 
-var history = createBrowserHistory()
-
-React.render((
-	<Router history={history}>
+ReactDOM.render((
+	<Router history={hashHistory}>
 		<Route path="/(:getHref)" component={App} />
 	</Router>
 ), document.body);
